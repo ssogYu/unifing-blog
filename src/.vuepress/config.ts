1729f98adc9f5801f1e73cc1d43746a2122bec60
shunsong.yu <shunsong.yu@ly.com>
2024-05-11 15:06:18 +0800

    update:增加百度网页访问统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,5 +7,19 @@ export default defineUserConfig({
   title: "ssogYu'blog",
   description: "技术分享",
   dest: "build",
+  head: [
+    [
+      "script",
+      {},
+      `var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?37f7e60310151c72959ddaef4ef7c45d";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `,
+    ],
+  ],
   theme,
 });
